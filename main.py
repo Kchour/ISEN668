b5aabcf91c4069452cba9624b6d3d115d3bffe92
Kenny <ckennyc@tamu.edu>
2020-04-28 23:17:57 -0500

    added transit to cmc, generated ship schedules

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import pdb
 from utils import *
 from search_algorithms import *
 import networkx as nx
-
+import random as rdm
+import numpy as np
 
 #### LOAD MISSION SET
 path = "./mission_set.ods"
@@ -17,7 +18,7 @@ dfCMC = pd.read_excel(path, sheet_name=0, engine='odf')
 
 #### LOAD SHIP SET
 path = "./ships.ods"
-dfShips = pd.read_excel(path, engine='odf', index_col="Ship")
+dfShips = pd.read_excel(path, engine='odf', index_col="Name")
 
 ### LOAD REGION SET
 path = "./region.ods"
@@ -54,24 +55,79 @@ edgeDict.update(edgeDictMirror)
 
 #### GENERATE SCHEDULES
 ''' We will use Dijkstra repeatedly to get feasible schedules for each ship
-    Inputs:
-    Planning horizon: 15 days
-    Planning time step: 1 day
-
+    Assumption:
+    -Planning horizon: 15 days
+    -Planning time step: 1 day
+    -Ship can participate in region if arrives with 16.0 hrs of day remaining
+    
+    Also:
+    -When in Transit, add rTransit region
 '''
-# USE dijkstra algorithm (astar with 0 heuristic)
+ship_schedule = {index:[] for index, info in dfShips.iterrows() if info["Avail"] == 'x'}
+# convert ship info to dict for ease of use
+dfShips_dict = dfShips.to_dict()
+
+# Initialize ship_schedule dict
+ship_schedule = {}
+ship_speed = 16  #knots
+cutoff_time = 16.0  # hrs must remain on arrival
+schedule_limit = 10 # limit the number of schedules generated
+# Create a graph based on edges given
 graph = MyGraph(edgeDict)
-print(graph.neighbors('r1'))
+for index, info in dfShips.iterrows():
+   
+    # USE dijkstra algorithm (astar with 0 heuristic)
+    #print(graph.neighbors('r1'))
+    iter_ = 1
+    full_schedule = []  #full schedule for one ship
+    while iter_< schedule_limit: 
+        # temporary schedule list
+        start = info["Start Region"]
+        temp = [start]
+        while True:
+            goal = None
+            SA = Search(graph, start, goal)
+            parents, optimal_cost = SA.a_star_search(h_type='zero', visualize=False)
+            ## RECONSTRUCT THE PATH
+            #path = reconstruct_path(parents, start, goal)
+           
+            # Random selection
+            rand_sel = rdm.choice(list(optimal_cost.keys())) 
 
-start = 'r1'
-goal = None
-SA = Search(graph, start, goal)
-parents, optimal_cost = SA.a_star_search(h_type='zero', visualize=False)
-## RECONSTRUCT THE PATH
-#path = reconstruct_path(parents, start, goal)
+            # Find Transit Time in days and decide what to do
+            time = optimal_cost[rand_sel]/ship_speed/24.0
+            
+            # round down if fractional part is less than 8 hours
+            if time-np.floor(time) <= 1/3:
+                time = np.floor(time).astype('int')
+            else:
+                time = np.ceil(time).astype('int')
+
+            for i in range(time):
+                temp.append('rTransit')
+            # Append the desired selection
+            temp.append(rand_sel)
+           
+            # start search from previous desired selection
+            start = rand_sel
+
+            # if temp is greater than 15 stop
+            if len(temp) > 15:
+                temp = temp[0:15]
+                break
+        # update current ship schedule
+        full_schedule.append(temp[:])
+        
+        #update iter_
+        iter_ +=1
+    pdb.set_trace()   
+    
+    # assign to each ship name (given by index) candidate schedules
+    ship_schedule.update({index: full_schedule[:]})
+    
+    
+    break
 pdb.set_trace()
-
-
 
 
 
