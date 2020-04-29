ac017739288e2d15e694605468ffdbf53e6aa430
Kenny <ckennyc@tamu.edu>
2020-04-29 03:39:03 -0500

    starting modeling with pyomo

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ dfMission =pd.read_excel(path, sheet_name=0, engine='odf', index_col='Mission')
 
 #### LOAD CMC SET
 path = "./cmc.ods"
-dfCMC = pd.read_excel(path, sheet_name=0, engine='odf')
+# need to forward fill for merged cells in the index
+dfCMC = pd.read_excel(path, sheet_name=0, engine='odf').fillna(method='ffill')
 
 #### LOAD SHIP SET
 path = "./ships.ods"
@@ -22,14 +23,19 @@ dfShips = pd.read_excel(path, engine='odf', index_col="Name")
 
 ### LOAD REGION SET
 path = "./region.ods"
-dfRegion = pd.read_excel(path, engine='odf', sheet_name=1)
+dfRegions = pd.read_excel(path, engine='odf', sheet_name=0)
+dfArcs = pd.read_excel(path, engine='odf', sheet_name=1)
+
+#### CREATE Dictionaries for ease of use
+#dfMissionDict = dfMission.to_dict('index')
+#dfCMC = 
 
 # Build list and  dictionary of edges. Should be undirected!
 # lists are not hashable types, thus cannot be dictionary keys
-edgeList = [(dfRegion.loc[i]['Arcs'], dfRegion.loc[i]['Unnamed: 1'], dfRegion.loc[i]['Length(nm)']) for i in range(len(dfRegion))]
+edgeList = [(dfArcs.loc[i]['Arcs'], dfArcs.loc[i]['Unnamed: 1'], dfArcs.loc[i]['Length(nm)']) for i in range(len(dfArcs))]
 
-edgeDict = {(dfRegion.loc[i]['Arcs'], dfRegion.loc[i]['Unnamed: 1']): dfRegion.loc[i]['Length(nm)'] for i in range(len(dfRegion))  }
-edgeDictMirror = {(dfRegion.loc[i]['Unnamed: 1'], dfRegion.loc[i]['Arcs']): dfRegion.loc[i]['Length(nm)'] for i in range(len(dfRegion))  }
+edgeDict = {(dfArcs.loc[i]['Arcs'], dfArcs.loc[i]['Unnamed: 1']): dfArcs.loc[i]['Length(nm)'] for i in range(len(dfArcs))  }
+edgeDictMirror = {(dfArcs.loc[i]['Unnamed: 1'], dfArcs.loc[i]['Arcs']): dfArcs.loc[i]['Length(nm)'] for i in range(len(dfArcs))  }
 edgeDict.update(edgeDictMirror)
 
 
@@ -52,89 +58,167 @@ edgeDict.update(edgeDictMirror)
 #plt.axis('off')
 #plt.show()
 
-
-#### GENERATE SCHEDULES
-''' We will use Dijkstra repeatedly to get feasible schedules for each ship
-    Assumption:
-    -Planning horizon: 15 days
-    -Planning time step: 1 day
-    -Ship can participate in region if arrives with 16.0 hrs of day remaining
-    
-    Also:
-    -When in Transit, add rTransit region
-'''
-ship_schedule = {index:[] for index, info in dfShips.iterrows() if info["Avail"] == 'x'}
-# convert ship info to dict for ease of use
-dfShips_dict = dfShips.to_dict()
-
-# Initialize ship_schedule dict
-ship_schedule = {}
-ship_speed = 16  #knots
-cutoff_time = 16.0  # hrs must remain on arrival
-schedule_limit = 10 # limit the number of schedules generated
-# Create a graph based on edges given
-graph = MyGraph(edgeDict)
-for index, info in dfShips.iterrows():
-   
-    # USE dijkstra algorithm (astar with 0 heuristic)
-    #print(graph.neighbors('r1'))
-    iter_ = 1
-    full_schedule = []  #full schedule for one ship
-    while iter_< schedule_limit: 
-        # temporary schedule list
-        start = info["Start Region"]
-        temp = [start]
-        while True:
-            goal = None
-            SA = Search(graph, start, goal)
-            parents, optimal_cost = SA.a_star_search(h_type='zero', visualize=False)
-            ## RECONSTRUCT THE PATH
-            #path = reconstruct_path(parents, start, goal)
-           
-            # Random selection
-            rand_sel = rdm.choice(list(optimal_cost.keys())) 
-
-            # Find Transit Time in days and decide what to do
-            time = optimal_cost[rand_sel]/ship_speed/24.0
-            
-            # round down if fractional part is less than 8 hours
-            if time-np.floor(time) <= 1/3:
-                time = np.floor(time).astype('int')
-            else:
-                time = np.ceil(time).astype('int')
-
-            for i in range(time):
-                temp.append('rTransit')
-            # Append the desired selection
-            temp.append(rand_sel)
-           
-            # start search from previous desired selection
-            start = rand_sel
-
-            # if temp is greater than 15 stop
-            if len(temp) > 15:
-                temp = temp[0:15]
-                break
-        # update current ship schedule
-        full_schedule.append(temp[:])
+def generate_schedules(dfShips, regenerate=False, saveToDisk=False):
+    #### GENERATE SCHEDULES
+    ''' We will use Dijkstra repeatedly to get feasible schedules for each ship
+        Condition:
+        if GEN_SCHEDULES == True, then we will regenerate schedules, else
+                                  read from disk
+       
+        Assumption:
+        -Planning horizon: 15 days
+        -Planning time step: 1 day
+        -Ship can participate in region if arrives with 16.0 hrs of day remaining
         
-        #update iter_
-        iter_ +=1
-    pdb.set_trace()   
+        Also:
+        -When in Transit, add rTransit region
+    '''
+    if regenerate==True:
+        ship_schedule = {index:[] for index, info in dfShips.iterrows() if info["Avail"] == 'x'}
+        # convert ship info to dict for ease of use
+        dfShips_dict = dfShips.to_dict()
+
+        # Initialize ship_schedule dict
+        ship_schedule = {}
+        ship_speed = 16  #knots
+        cutoff_time = 16.0  # hrs must remain on arrival
+        schedule_limit = 10 # limit the number of schedules generated
+        # Create a graph based on edges given
+        graph = MyGraph(edgeDict)
+
+        ship_inst = 1
+        for index, info in dfShips.iterrows():
+           
+            # USE dijkstra algorithm (astar with 0 heuristic)
+            #print(graph.neighbors('r1'))
+            iter_ = 0
+            full_schedule = []  #full schedule for one ship
+            while iter_<= schedule_limit: 
+                # temporary schedule list
+                start = info["Start Region"]
+                temp = [start]
+                while True:
+                    goal = None
+                    SA = Search(graph, start, goal)
+                    parents, optimal_cost = SA.a_star_search(h_type='zero', visualize=False)
+                    ## RECONSTRUCT THE PATH
+                    #path = reconstruct_path(parents, start, goal)
+                   
+                    # Random selection
+                    rand_sel = rdm.choice(list(optimal_cost.keys())) 
+
+                    # Find Transit Time in days and decide what to do
+                    time = optimal_cost[rand_sel]/ship_speed/24.0
+                    
+                    # round down if fractional part is less than 8 hours
+                    if time-np.floor(time) <= 1/3:
+                        time = np.floor(time).astype('int')
+                    else:
+                        time = np.ceil(time).astype('int')
+
+                    for i in range(time):
+                        temp.append('rTransit')
+                    # Append the desired selection
+                    temp.append(rand_sel)
+                   
+                    # start search from previous desired selection
+                    start = rand_sel
+
+                    # if temp is greater than 15 stop
+                    if len(temp) > 15:
+                        temp = temp[0:15]
+                        break
+                # update current ship schedule
+                full_schedule.append(temp[:])
+                
+                #update iter_
+                iter_ +=1
+            
+            # assign to each ship name (given by index) candidate schedules
+            ship_schedule.update({index: full_schedule[:]})
+           
+            # if testing just 1 ship
+            if ship_inst >= 3:
+                break
+            ship_inst += 1
+    else:
+        #READ schedules from disk if they exist
+        test = pd.read_excel("testSchedule.xls")
+        ship_schedule = test.to_dict('list')
+
+        # remote 'quotes' from read schedules
+        for item in ship_schedule.keys():
+            temp = []
+            for subitem in ship_schedule[item]:
+                temp.append(eval(subitem))
+            ship_schedule.update({item: temp})
+
+    # Save schedules to disk
+    if saveToDisk == True and regenerate == True:
+        dfSave = pd.DataFrame.from_dict(ship_schedule)
+        dfSave.to_excel("testSchedule.xls", index=False)
     
-    # assign to each ship name (given by index) candidate schedules
-    ship_schedule.update({index: full_schedule[:]})
+    return ship_schedule
+
+#### RETRIEVE CANDIDATE SCHEDULES
+ship_schedule = generate_schedules(dfShips, regenerate=False, saveToDisk=False)
+
+#### BUILD MODEL FOR SOLVING
+''' TO debug model do:
+    model.pprint()
+'''
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+
+
+## Create some variables for ease of use
+days = np.arange(1,16).tolist()      
+regions = dfRegions['Region'].tolist()
+
+# Create Value data
+#[(m,n,r,d) for m in 
+
+# only select cmcs from the dfShips list
+cmcS = {}
+cmcCols = dfCMC.columns.tolist()
+for ship in ship_schedule.keys():
+
+    # GET ALL THE CMCS for current ship 
+    cmcTemp = [dfShips.loc[ship]['CMC']]
+    cmcTemp.append(dfShips.loc[ship]['Unnamed: 8'])
+    cmcTemp.append(dfShips.loc[ship]['Unnamed: 9'])
     
-    
-    break
+    #collect cmcs
+    tempArray=[]
+    for cmcValue in cmcTemp:
+        #remove ship class column
+        row = dfCMC.loc[(dfCMC['CMC']==cmcValue) ][cmcCols[1::]]
+        tempArray.append(row.to_dict('records')[0])
+
+    # APPEND TRANSIT CMC
+    row = dfCMC.loc[(dfCMC['Ship Class']=='ALL SHIPS')][cmcCols[1::]]
+    tempArray.append(row.to_dict('records')[0])
+
+    # add all cmcs to current ship
+    cmcS.update({ship: {'Class': dfShips.loc[ship]['Class'], 'ALL_CMCs': tempArray[:]}})
+
+#cmcS = {ship: info for ship in ship_schedule.keys() for index, info in dfCMC.iterrows() if info['Ship Class']==dfShips.loc[ship]['Class'] or info['Ship Class'] == 'All SHIPS'}
+# initialize model
+model = ConcreteModel()
+
+# binary variable for schedule selected for ship
+model.schedule = Var( ((ship, schedule) for ship in ship_schedule.keys() for schedule in range(len(ship_schedule[ship]))), within=Binary, initialize=0)
+
+# binary variables for concurrent mission selection. Indexed by ship, cmc, day, and region
 pdb.set_trace()
+model.cmc = Var( ((ship, cmc['CMC'], day, region) for ship in ship_schedule.keys() for cmc in cmcS[ship]['ALL_CMCs'] for day in days for region in regions), within=Binary, initialize=0)
 
+#for index, row in dfCMC.iterrows():
+#    if row["Ship Class"] == prevClass:
+#        temp
 
-
-
-
-
-
+#### TIMING CODE
+from codetiming import Timer
 
 
 
